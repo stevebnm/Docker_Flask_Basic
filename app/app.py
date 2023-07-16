9e691fbbe3e5a8a7b057221136153dcc656a1171
Swastik <96874651+stevebnm@users.noreply.github.com>
2023-07-16 22:04:03 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,66 +8,113 @@ import mysql.connector
 import json
 
 app = Flask(__name__,template_folder="templates")
+
+
 # Connecting to Sql
-config = {
-    'user': 'root',
-    'password':  'root',
-    'host': 'db',
-    'port': '3306',
-    'database': 'Employee'
-}
 
-try:
+@app.route('/')
+def version():
+  config = {
+      'user': 'root',
+      'password': 'root',
+      'host': 'db',
+      'port': '3306',
+      'database': 'Employee'
+  }
+  try:
+      connection = mysql.connector.connect(**config)
+      cursor = connection.cursor()
+      db_Info = connection.get_server_info()
+      cursor.close()
+      connection.close()
+      return "Connected to MySQL Server {}".format(db_Info)+"<br>"+"If you want to insert users to MySQL, access with /insert"
+  except:
+      return "Oops!! Unable to Connect to MySQL DB"
+
+def display_all():
+  config = {
+      'user': 'root',
+      'password': 'root',
+      'host': 'db',
+      'port': '3306',
+      'database': 'Employee'
+  }
+
   connection = mysql.connector.connect(**config)
+
+  # Get All Employees
   cursor = connection.cursor()
-except:
-  print("Oops!! Unable to Connect to MySQL DB")
+  getEmployees = "select * from employee"
+  cursor.execute(getEmployees)
+  AllEmployees = cursor.fetchall()
+  connection.close()
+  return AllEmployees
 
-# Home Method
-@app.route("/", methods=['GET','POST'])
+
+@app.route("/home", methods=['GET','POST'])
 def home():
-    # Post request
-    if request.method == "POST":
+  AllEmployees = display_all()
+  config = {
+      'user': 'root',
+      'password': 'root',
+      'host': 'db',
+      'port': '3306',
+      'database': 'Employee'
+  }
 
-        # Get Data from Form
-        name = request.form['name']
-        job = request.form['job']
-        salary = request.form['salary']
+  # Post request
+  if request.method == "POST":
 
-        # Insert into employee Table
-        addEmployee = ("INSERT INTO employee "
-              "(name, job, salary) "
-              "VALUES (%(name)s, %(job)s, %(salary)s)")
-        
-        dataEmployee = {
-            'name': name,
-            'job': job,
-            'salary': salary,
-        }
-        cursor.execute(addEmployee,dataEmployee)
-        connection.commit()
 
-    # Get All Employees
-    getEmployees = "select * from employee"
-    cursor.execute(getEmployees)
-    AllEmployees = cursor.fetchall()
+      # Get Data from Form
+      name = request.form['name']
+      job = request.form['job']
+      salary = request.form['salary']
 
-    # Render Home Page
-    return render_template('home.html',allEmployees=AllEmployees)
+      # Insert into employee Table
+      addEmployee = ("INSERT INTO employee "
+            "(name, job, salary) "
+            "VALUES (%(name)s, %(job)s, %(salary)s)")
+
+      dataEmployee = {
+          'name': name,
+          'job': job,
+          'salary': salary,
+      }
+      connection = mysql.connector.connect(**config)
+      cursor = connection.cursor()
+      cursor.execute(addEmployee,dataEmployee)
+      connection.commit()
+
+
+  # Render Home Page
+  return render_template('home.html',allEmployees=AllEmployees)
+
+
 
 # Delete Method
 @app.route("/delete", methods=['POST'])
 def delete():
+  config = {
+      'user': 'root',
+      'password': 'root',
+      'host': 'db',
+      'port': '3306',
+      'database': 'Employee'
+  }
 
-    # Get ID from form
-    id_ = (request.form['id'],)
-    # Delete From employee Table
-    deleteEmployee = """ Delete from employee where id = %s """
-    cursor.execute(deleteEmployee,id_)
-    connection.commit()
+  connection = mysql.connector.connect(**config)
+  cursor = connection.cursor()
 
-    # Redirect to Home page
-    return redirect(url_for('home'))
+  # Get ID from form
+  id_ = (request.form['id'],)
+  # Delete From employee Table
+  deleteEmployee = """ Delete from employee where id = %s """
+  cursor.execute(deleteEmployee,id_)
+  connection.commit()
+
+  # Redirect to Home page
+  return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
+  app.run(host="0.0.0.0",debug=True)
